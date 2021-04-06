4935eaba262f9e58254f8f89fe95f1a1af1119dd
poke1646 <meergujral@gmail.com>
2021-04-06 04:06:41 -0400

    Some Front End Code built in React

---
+++ Project/about.component.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class About extends Component {
+    render() {
+        return (
+            <div class="container">
+                <h3>Welcome To About Page</h3>
+        </div>
+        )
+    }
+}
